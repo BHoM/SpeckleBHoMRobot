490396d0365230d004e30b9f0581943c13105e92
Alessio Lombardi <alessio.lombardi@burohappold.com>
2019-12-23 18:08:13 +0000

    First go

---
+++ CommandClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RobotOM;
+
+namespace SpeckleRobotClient
+{
+    // Created following the guide here: 
+    // https://forums.autodesk.com/t5/robot-structural-analysis-forum/creating-dll-for-api-and-making-it-available-for-robot-menu/m-p/3359313
+
+    [System.Runtime.InteropServices.ComVisibleAttribute(true), System.Runtime.InteropServices.Guid("D6A60BA1-1C40-4E5E-A6A3-293D325ACD17")]
+    public class CommandClass : IRobotAddIn
+    {
+        private IRobotApplication iapp = null;
+        public bool Connect(RobotApplication robot_app, int add_in_id, bool first_time)
+        {
+            iapp = robot_app;
+            return true;
+        }
+
+        public bool Disconnect()
+        {
+            iapp = null;
+            return true;
+        }
+
+        public void DoCommand(int cmd_id)
+        {
+            System.Windows.Forms.MessageBox.Show("Command " + cmd_id.ToString() + " executed.");
+        }
+
+        public double GetExpectedVersion()
+        {
+            return 10;
+        }
+
+        public int InstallCommands(RobotCmdList cmd_list)
+        {
+            cmd_list.New(1, "My Command 1");
+            return cmd_list.Count;
+        }
+    }
+}
---
+++ InitialiseBrowser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CefSharp;
+using System.Reflection;
+using CefSharp.WinForms;
+using System.Net;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SpeckleRobotClient
+{
+    static class InitialiseBrowser
+    {
+        public static ChromiumWebBrowser Browser;
+
+        public static void InitializeCef()
+        {
+            if (Cef.IsInitialized) return;
+
+            Cef.EnableHighDPISupport();
+
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            var assemblyPath = Path.GetDirectoryName(assemblyLocation);
+            var pathSubprocess = Path.Combine(assemblyPath, "CefSharp.BrowserSubprocess.exe");
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            var settings = new CefSettings
+            {
+                BrowserSubprocessPath = pathSubprocess
+            };
+
+            settings.CefCommandLineArgs.Add("allow-file-access-from-files", "1");
+            settings.CefCommandLineArgs.Add("disable-web-security", "1");
+            Cef.Initialize(settings);
+        }
+
+        public static void InitializeChromium()
+        {
+            if (Browser != null && !Browser.IsDisposed) return;
+
+#if DEBUG
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://localhost:9090/");
+            request.Timeout = 100;
+            request.Method = "HEAD";
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+                var copy = response;
+                Browser = new ChromiumWebBrowser(@"http://localhost:9090/");
+            }
+            catch (WebException)
+            {
+                //Browser = new ChromiumWebBrowser(@"http://localhost:9090/");
+                // IF DIMITRIE ON PARALLELS
+                Browser = new ChromiumWebBrowser(@"http://10.211.55.2:9090/");
+            }
+#else
+      var path = Directory.GetParent( Assembly.GetExecutingAssembly().Location );
+      Debug.WriteLine( path, "SPK" );
+
+      var indexPath = string.Format( @"{0}\app\index.html", path );
+
+      if ( !File.Exists( indexPath ) )
+        Debug.WriteLine( "Speckle for Rhino: Error. The html file doesn't exists : {0}", "SPK" );
+
+      indexPath = indexPath.Replace( "\\", "/" );
+
+      Browser = new ChromiumWebBrowser( indexPath );
+#endif
+
+            // Allow the use of local resources in the browser
+            Browser.BrowserSettings = new BrowserSettings
+            {
+                FileAccessFromFileUrls = CefState.Enabled,
+                UniversalAccessFromFileUrls = CefState.Enabled
+            };
+
+            Browser.Dock = DockStyle.Fill;
+        }
+    }
+}
