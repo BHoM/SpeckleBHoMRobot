f7a4a6c1bdf4f96c3a4459640d87b414a5b17f79
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-02-19 14:33:24 +0000

    Copy SpeckleClientsWrapper from SpeckleRevitReboot

---
+++ HelperClasses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SpeckleRobotClient
+{
+    /// <summary>
+    /// Wrapper class to manage the storage of speckle clients.
+    /// </summary>
+    public class SpeckleClientsWrapper
+    {
+        public List<dynamic> clients { get; set; }
+
+        public SpeckleClientsWrapper() { clients = new List<dynamic>(); }
+
+        public List<string> GetStringList()
+        {
+            var myList = new List<string>();
+            foreach (dynamic el in clients)
+            {
+                myList.Add(JsonConvert.SerializeObject(el));
+            }
+            return myList;
+        }
+
+        public void SetClients(IList<string> stringList)
+        {
+            clients = stringList.Select(el => JsonConvert.DeserializeObject<dynamic>(el)).ToList();
+        }
+    }
+}
