ecbe753297e9ce5aea4fc74470b46b056049e799
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-03-06 22:15:39 +0000

    SpeckleClientsSchema

--- HelperClasses.cs
+++ HelperClasses.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RobotOM;
 using Newtonsoft.Json;
 
 namespace SpeckleRobotClient
@@ -29,6 +30,21 @@ namespace SpeckleRobotClient
         public void SetClients(IList<string> stringList)
         {
             clients = stringList.Select(el => JsonConvert.DeserializeObject<dynamic>(el)).ToList();
+        }
+    }
+
+    public static class SpeckleClientsSchema
+    {
+        private static IRobotParamSchemaMngr schemaMngr;
+
+        public static IRobotParamSchema GetSchema()
+        {
+            //If the schema with this name already exists, the function returns its definition.
+            IRobotParamSchema schema = schemaMngr.Create("SpeckleClientStorage");
+            
+            schema.Def.AddSimpleParam("clients", null);
+
+            return schema;
         }
     }
 }
