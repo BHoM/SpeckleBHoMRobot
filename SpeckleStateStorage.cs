08e473dba91512d9fbda6a2d7b1c97f04856e594
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-03-07 18:48:03 +0000

    get schema manager from current project

--- SpeckleStateStorage.cs
+++ SpeckleStateStorage.cs
@@ -13,9 +13,10 @@ namespace SpeckleRobotClient
     {
         //not sure if Guid is necessary or if it's specific to revit (need it to construct revit schema)
         //readonly static Guid schemaGuid = new Guid("{5A447395-5833-4BE4-B79D-5006AD81D704}");
-        private static IRobotParamSchemaMngr schemaMngr;
-        public static IRobotParamSchema GetSchema( )
+        public static IRobotParamSchema GetSchema( IRobotProject doc )
         {
+            IRobotParamSchemaMngr schemaMngr = doc.Structure.ExtParams.Schemas;
+
             if (schemaMngr.Exist("SpeckleLocalStateStorage")) 
                 return schemaMngr.GetByName("SpeckleLocalStateStorage");
          
@@ -29,11 +30,13 @@ namespace SpeckleRobotClient
 
     static class UniqueSchemaLocalState
     {
-        private static IRobotParamSchemaMngr schemaMngr;
-
-        public static IRobotParamSchema GetSchema()
+        public static IRobotParamSchema GetSchema(IRobotProject doc)
         {
-            //If the schema with this name already exists, the function returns its definition.
+            IRobotParamSchemaMngr schemaMngr = doc.Structure.ExtParams.Schemas;
+
+            if (schemaMngr.Exist("UniqueSchemaLocalState"))
+                return schemaMngr.GetByName("UniqueSchemaLocalState");
+
             IRobotParamSchema schema = schemaMngr.Create("UniqueSchemaLocalState");
 
             schema.Def.AddSimpleParam("guid", null);
@@ -45,23 +48,21 @@ namespace SpeckleRobotClient
     public static class SpeckleStateManager
     {
         readonly static Guid ID = new Guid("{C44D581C-DECC-492A-8CEC-DA3F7D3A2802}");
-        private static IRobotParamSchemaMngr schemaMngr;
         private static IRobotParamCollection paramCollection;
-        private static IRobotNodeServer nodeServer;
 
         public static void WriteState(IRobotProject doc, List<SpeckleStream> state)
         {
             //not sure what this data looks like so this might not work; just throwin this in for the mo
             string ls = string.Join(",", state.Select(stream => JsonConvert.SerializeObject(stream)).ToList());
 
-            IRobotParamSchema stateSchema = SpeckleStateSchema.GetSchema();
+            IRobotParamSchema stateSchema = SpeckleStateSchema.GetSchema(doc);
 
             //oh nooo a prob here -- can't set to whole proj b/c doc ID is str while structure ID is long 😑
             //needa think of somewhere else to put this
             //stateSchema.SetParam(doc.UniqueId, "streams", ls);
 
             //this'll do for now -- making a temp node to attach the local state to
-
+            IRobotNodeServer nodeServer = doc.Structure.Nodes;
             if (nodeServer.Exist(SpeckleUiBindingsRobot.node_id) == 0) nodeServer.Create(SpeckleUiBindingsRobot.node_id, 0, 0, 0);
 
             IRobotDataObject speckNode = nodeServer.Get(SpeckleUiBindingsRobot.node_id);
@@ -87,7 +88,7 @@ namespace SpeckleRobotClient
         //not sure where this fits yet; `GetStateEntity` in revit plugin, but there isn't really an entity equiv in robot
         public static IRobotParamSchema GetParamSchema(IRobotProject doc)
         {
-            IRobotParamSchema stateParam = SpeckleStateSchema.GetSchema();
+            IRobotParamSchema stateParam = SpeckleStateSchema.GetSchema(doc);
 
             return null;
         }
