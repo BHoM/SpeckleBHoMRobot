ebc925caef1714509b24fe10a149151b9d6fb5a6
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-03-07 12:16:25 +0000

    some adapted copypasta from revit plugin

--- SpeckleUiBindings.cs
+++ SpeckleUiBindings.cs
@@ -38,6 +38,59 @@ namespace SpeckleRobotClient
             ClientListWrapper = new SpeckleClientsWrapper();
         }
 
+        public void InitialiseBinding()
+        {
+            // LOCAL STATE
+            LocalState = new List<SpeckleStream>();
+
+            Queue.Add(new Action(() =>
+            {
+                LocalState = SpeckleStateManager.ReadState(Project);
+                InjectStateInKits();
+            }));
+
+            SelectionTimer = new Timer(1400) { AutoReset = true, Enabled = true };
+            SelectionTimer.Elapsed += SelectionTimer_Elapsed;
+        }
+
+        private void SelectionTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (Project == null) return;
+            var selectedObjectsCount = 0;
+            foreach (var i in Enumerable.Range(-2,10))
+            {
+                selectedObjectsCount += Project.Structure.Selections.Get((IRobotObjectType)(i)).Count;
+            }
+
+            NotifyUi("update-selection-count", JsonConvert.SerializeObject(new
+            {
+                selectedObjectsCount
+            }));
+        }
+
+        /// <summary>
+        /// Injects the current lolcal state in any speckle kit initialiser class that has a "LocalRobotState" property defined. 
+        /// This can then be used to determine what existing speckle baked objects exist in the current doc and either modify/delete whatever them in the conversion methods.
+        /// </summary>
+        public void InjectStateInKits()
+        {
+            var assemblies = SpeckleCore.SpeckleInitializer.GetAssemblies();
+            foreach (var ass in assemblies)
+            {
+                var types = ass.GetTypes();
+                foreach (var type in types)
+                {
+                    if (type.GetInterfaces().Contains(typeof(SpeckleCore.ISpeckleInitializer)))
+                    {
+                        if (type.GetProperties().Select(p => p.Name).Contains("LocalRobotState"))
+                        {
+                            type.GetProperty("LocalRobotState").SetValue(null, LocalState);
+                        }
+                    }
+                }
+            }
+        }
+
         public override void AddObjectsToSender(string args)
         {
             throw new NotImplementedException();
