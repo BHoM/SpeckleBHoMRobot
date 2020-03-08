1c8ab7fe791b244f8a7a4cc6651c554d1090becf
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-03-08 18:52:54 +0000

    pasted in SpeckleError from SpeckleCore.Data was getting a MissingMethodException when initialising speckle after updating to v1.7, so reverted back to v1.6.9 for now until i can investigate it further

---
+++ SpeckleError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeckleRobotClient
+{
+    // getting MissingMethodException when upgrading SpeckleCore, so just pasting this in here for now
+    public class SpeckleError
+    {
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+    public class SpeckleConversionError : SpeckleError
+    {
+        public object SourceObject { get; set; }
+    }
+}
