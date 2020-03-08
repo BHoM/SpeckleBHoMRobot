923154c7499fa6f152414bf26f2087965d9ff589
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-03-08 15:14:38 +0000

    debug browser

--- SpecklePlugin.cs
+++ SpecklePlugin.cs
@@ -64,22 +64,23 @@ namespace SpeckleRobotClient
             if (Browser != null && !Browser.IsDisposed) return;
 
 #if DEBUG
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://localhost:9090/");
-            request.Timeout = 100;
-            request.Method = "HEAD";
-            HttpWebResponse response;
-            try
-            {
-                response = (HttpWebResponse)request.GetResponse();
-                var copy = response;
-                Browser = new ChromiumWebBrowser(@"http://localhost:9090/");
-            }
-            catch (WebException)
-            {
-                //Browser = new ChromiumWebBrowser(@"http://localhost:9090/");
-                // IF DIMITRIE ON PARALLELS
-                Browser = new ChromiumWebBrowser(@"http://10.211.55.2:9090/");
-            }
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://localhost:9090/");
+            //request.Timeout = 100;
+            //request.Method = "HEAD";
+            //HttpWebResponse response;
+            //try
+            //{
+            //    response = (HttpWebResponse)request.GetResponse();
+            //    var copy = response;
+            //    Browser = new ChromiumWebBrowser(@"http://localhost:9090/");
+            //}
+            //catch (WebException)
+            //{
+            //    //Browser = new ChromiumWebBrowser(@"http://localhost:9090/");
+            //    // IF DIMITRIE ON PARALLELS
+            //    Browser = new ChromiumWebBrowser(@"http://10.211.55.2:9090/");
+            //}
+            Browser = new ChromiumWebBrowser(@"https://hestia.speckle.works/signin");
 #else
             //var path = Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString();
             //Debug.WriteLine(path, "SPK");
@@ -91,8 +92,7 @@ namespace SpeckleRobotClient
 
             //indexPath = indexPath.Replace("\\", "/");
 
-            //Browser = new ChromiumWebBrowser(indexPath);
-            Browser = new ChromiumWebBrowser(@"https://hestia.speckle.works/signin");
+            //Browser = new ChromiumWebBrowser(indexPath);         
 #endif
 
             // Allow the use of local resources in the browser
